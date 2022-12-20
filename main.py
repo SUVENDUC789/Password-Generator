a43a8fb438a01173475d24d02389e3c297e445b1
Suvendu <suvendu.chok@gmail.com>
2022-12-20 15:49:06 +0530

    using List Comprehension

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
 import random
 
+def main():
+    small = [chr(i) for i in range(97, 123)]
+    big = [chr(i) for i in range(65, 91)]
+    number = [chr(i) for i in range(48, 58)]
+    special = [chr(i) for i in range(35, 39)]+list(chr(64))
 
-def listGenerator(start, end):
-    a = []
-    for i in range(start, end):
-        a.append(chr(i))
-    return a
-
-
-if __name__ == '__main__':
-    small = listGenerator(97, 123)
-    big = listGenerator(65, 91)
-    number = listGenerator(48, 58)
-    special = listGenerator(35, 39)+list(chr(64))
     try:
         n = int(input("Enter size of your password : "))
         i = 0
@@ -21,26 +14,30 @@ if __name__ == '__main__':
             item = random.choice(small)
             passW += item
             i = i+1
-            if n==i:
+            if n == i:
                 break
             item = random.choice(big)
             passW += item
             i = i+1
-            if n==i:
+            if n == i:
                 break
             item = random.choice(number)
             passW += item
             i = i+1
-            if n==i:
+            if n == i:
                 break
             item = random.choice(special)
             passW += item
             i = i+1
-            if n==i:
+            if n == i:
                 break
-        print("Your Password length is :",len(passW),"\n")
-        print(passW,end='\n')
+        print("Your Password length is :", len(passW), "\n")
+        print(passW, end='\n')
     except Exception as e:
         print("Note : ", e, " Please enter number ")
 
     input("\nPress any key to continue ...")
+
+
+if __name__ == '__main__':
+    main()
